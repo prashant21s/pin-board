7dc9d1094be404db8a6fc1b453b379089e377fb0
prashant21s <prashant2311059@akgec.ac.in>
2026-08-14 22:39:55 +0530

    update frontend api url

--- pinterest-frontend/src/api/axios.js
+++ pinterest-frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
-  withCredentials: true // ye zaroori hai — session cookie bhejne/receive karne ke liye
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true
 });
 
 export default api;
